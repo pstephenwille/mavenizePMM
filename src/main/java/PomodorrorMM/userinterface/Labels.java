ef8d8b45968f93635e18bcfec504e8a843831f03
Bloomfield, Keith (HAL Contractor) <KBloomfield@HollandAmerica.com>
2014-12-01 10:46:17 -0800

    Removing double semicolons

--- src/main/java/PomodorrorMM/userinterface/Labels.java
+++ src/main/java/PomodorrorMM/userinterface/Labels.java
@@ -14,14 +14,14 @@ public class Labels {
     private static final String TXT_PERCENTAGE = " %";
     private static final String TXT_SET_OPACITY = "Set opacity to ";
 
-    public static final Label BREAK_TIMER = new Label();;
-    public static final Label DURATION_BREAK = new Label(TXT_DURATION_BREAK);;
-    public static final Label DURATION_WORK = new Label(TXT_DURATION_WORK);;
-    public static final Label INSTRUCTIONS = new Label(TXT_INSTRUCTIONS);;
-    public static final Label MINUTES_TENS = new Label(TXT_MINUTES_TENS);;
-    public static final Label MINUTES_ONES = new Label(TXT_MINUTES_ONES);;
-    public static final Label PERCENT = new Label(TXT_PERCENTAGE);;
-    public static final Label SET_OPACITY = new Label(TXT_SET_OPACITY);;
+    public static final Label BREAK_TIMER = new Label();
+    public static final Label DURATION_BREAK = new Label(TXT_DURATION_BREAK);
+    public static final Label DURATION_WORK = new Label(TXT_DURATION_WORK);
+    public static final Label INSTRUCTIONS = new Label(TXT_INSTRUCTIONS);
+    public static final Label MINUTES_TENS = new Label(TXT_MINUTES_TENS);
+    public static final Label MINUTES_ONES = new Label(TXT_MINUTES_ONES);
+    public static final Label PERCENT = new Label(TXT_PERCENTAGE);
+    public static final Label SET_OPACITY = new Label(TXT_SET_OPACITY);
 
     public static void updateBreakTimer(String minutesText, String secondsText, String millisText) {
         BREAK_TIMER.setText(minutesText + ":" +
